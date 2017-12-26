9586f4021b6c9ca808af5150495f07e1f71315a2
戚雨 <yuqi@wisedu.com>
2017-12-26 17:24:03 +0800

    解决require环境，微信sdk会加载两次出错的问题

--- src/wx/wx.js
+++ src/wx/wx.js
@@ -1,11 +1,24 @@
 import proxy from './proxy.js';
 
 export default (callback, config = {}, dconfig = {}) => {
-  let url = 'http://res.wx.qq.com/open/js/jweixin-1.2.0.js'; // 远端文件地址
-  var script = document.createElement("script");
-  script.src = url;
-  document.head.appendChild(script);
-  script.onload = () => {
+  if (window.require === undefined) {
+    let url = 'http://res.wx.qq.com/open/js/jweixin-1.2.0.js'; // 远端文件地址
+    var script = document.createElement("script");
+    script.src = url;
+    document.head.appendChild(script);
+    script.onload = () => {
+      wx.config(config);
+      wx.ready(() => {
+        var mobileSDK = {};
+        proxy(wx, mobileSDK, dconfig);
+        global.BH_MIXIN_SDK = mobileSDK;
+        callback({
+          type: 'success',
+          sdk: mobileSDK
+        });
+      });
+    };
+  } else {
     wx.config(config);
     wx.ready(() => {
       var mobileSDK = {};
@@ -16,5 +29,5 @@ export default (callback, config = {}, dconfig = {}) => {
         sdk: mobileSDK
       });
     });
-  };
+  }
 }
