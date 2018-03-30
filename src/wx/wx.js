93224259d43d6a434609341955ba0fe37c19340d
戚雨 <yuqi@wisedu.com>
2018-03-30 16:55:16 +0800

    支持https

--- src/wx/wx.js
+++ src/wx/wx.js
@@ -2,7 +2,7 @@ import proxy from './proxy.js';
 
 export default (callback, config = {}, dconfig = {}) => {
   if (window.require === undefined) {
-    let url = 'http://res.wx.qq.com/open/js/jweixin-1.2.0.js'; // 远端文件地址
+    let url = 'https://res.wx.qq.com/open/js/jweixin-1.2.0.js'; // 远端文件地址
     var script = document.createElement("script");
     script.src = url;
     document.head.appendChild(script);
