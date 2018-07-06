9c1c4733615a6b759c471c99309f29cca64869c1
[wangyongjian] <[1005934453@qq.com]>
2018-07-06 11:21:41 +0800

    关闭今日校园ios机型的弹性效果

--- src/bh/bh.js
+++ src/bh/bh.js
@@ -6,6 +6,7 @@ export default function(callback, config = {}) {
         if (global.BH_MOBILE_SDK) {
             var sdk = BH_MOBILE_SDK;
             var mobileSDK = {};
+            sdk.UI.setBouncesEnabled(false); // 关闭今日校园（ios机型）上的禁用弹性效果
             proxy(sdk, mobileSDK);
             global.BH_MIXIN_SDK = mobileSDK;
             callback({
