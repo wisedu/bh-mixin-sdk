cbffad61a824fe49159f0403aa42155ec4c72206
xujiabin <xujiabin0114@163.com>
2018-12-03 10:49:36 +0800

    调整分享

--- src/wx/wx.js
+++ src/wx/wx.js
@@ -26,7 +26,16 @@ export default (callback, config = {}, dconfig = {}) => {
       console.log(config)
       window.wx.config(config);
       window.wx.ready((ele) => {
-        console.log(ele);
+        // wx.checkJsApi({
+        //   jsApiList: ['onMenuShareTimeline','onMenuShareAppMessage'], // 需要检测的JS接口列表，所有JS接口列表见附录2,
+        //   success: function(res) {
+        //     // 以键值对的形式返回，可用的api值true，不可用为false
+        //     // 如：{"checkResult":{"chooseImage":true},"errMsg":"checkJsApi:ok"}
+        //     console.log('onMenuShareTimeline-onMenuShareAppMessage')
+        //     console.log(res)
+        //   }
+        // });
+        //console.log(ele);
         var mobileSDK = {};
         proxy(wx, mobileSDK, dconfig);
         global.BH_MIXIN_SDK = mobileSDK;
@@ -34,6 +43,36 @@ export default (callback, config = {}, dconfig = {}) => {
           type: 'success',
           sdk: mobileSDK
         });
+        // //测试临时代码
+        // var imgurl = 'http://ampdx2.wisedu.com/rsfw/sys/rsydzp/public/images/zwgg_d.png';
+        // var title = '我是测试啊';
+        // var url = 'http://ampdx2.wisedu.com/rsfw/sys/rsydzp/share/ee9239ad09034d5a993614d276d051a0.do';
+        // var desc = '我是测试啊我是测试啊';
+        // wx.onMenuShareTimeline({
+        //     title: title, // 分享标题
+        //     desc: desc, // 分享描述
+        //     link: url, // 分享链接
+        //     imgUrl: imgurl, // 分享图标
+        //     success: function () {
+        //         alert('成功')
+        //         // 用户确认分享后执行的回调函数
+        //     },
+        //     cancel: function () {
+        //         alert('失败')
+        //         // 用户取消分享后执行的回调函数
+        //     }
+        // });
+        // wx.onMenuShareAppMessage({
+        //   title: title, // 分享标题
+        //   desc: desc, // 分享描述
+        //   link: url, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+        //   imgUrl: imgurl, // 分享图标
+        //   type: '', // 分享类型,music、video或link，不填默认为link
+        //   dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
+        //   success: function () {
+        //     alert('分享朋友成功')
+        //   }
+        // });
       });
       window.wx.error(function(res){
         console.log('config信息验证失败会执行error函数');
