fcf9bba15a75d0dab6f7ffd22c2b004702d5864a
mxl <www_mxl@163.com>
2019-01-03 18:48:25 +0800

    此次修改主要集中在拍照及上传功能

--- src/lt/lt.js
+++ src/lt/lt.js
@@ -1,11 +1,48 @@
 import proxy from './proxy.js';
 
 export default (callback, config = {}, dconfig = {}) => {
-  let url = 'http://gitee.com/lantu/mc-sdk-insight-public/raw/v2.0.1/mc-sdk.2.0.1.js'; // 文件地址
-  var script = document.createElement("script");
-  script.src = url;
-  document.head.appendChild(script);
-  script.onload = () => {
+  // if (window.require === undefined) {
+  //   let url = 'https://gitee.com/lantu/mc-sdk-insight-public/raw/v2.0.1/mc-sdk.2.0.1.js'; // 文件地址
+  //   var script = document.createElement("script");
+  //   script.src = url;
+  //   document.head.appendChild(script);
+  //   script.onload = () => {
+  //     var appkey = "24e1aeb8";
+  //     MCK.auth(appkey);
+  //     MCK.ready(()=>{
+  //         // 验证通过回调, 此处可以放心调用API
+  //         // 这里 sdk 即是 MCK
+  //         // MCK === sdk, 为true
+  //         var mobileSDK = {};
+  //         proxy(MCK, mobileSDK, config);
+  //         global.BH_MIXIN_SDK = MCK;
+  //         callback({
+  //             type: 'lt-success',
+  //             sdk: mobileSDK
+  //         });
+      
+  //     });
+  //   };
+  // } else {
+  //   window.require(['LT'], function(MCK){
+  //     var appkey = "24e1aeb8";
+  //     MCK.auth(appkey);
+  //     MCK.ready(()=>{
+  //         // 验证通过回调, 此处可以放心调用API
+  //         // 这里 sdk 即是 MCK
+  //         // MCK === sdk, 为true
+  //         var mobileSDK = {};
+  //         proxy(MCK, mobileSDK, config);
+  //         global.BH_MIXIN_SDK = MCK;
+  //         callback({
+  //             type: 'lt-success',
+  //             sdk: mobileSDK
+  //         });
+      
+  //     });
+  //   });
+  // }
+  setTimeout(()=>{
     var appkey = "24e1aeb8";
     MCK.auth(appkey);
     MCK.ready(()=>{
@@ -20,7 +57,6 @@ export default (callback, config = {}, dconfig = {}) => {
             sdk: mobileSDK
         });
     
-      });
-
-  };
+    });
+  },0)
 }
--- src/lt/proxy.js
+++ src/lt/proxy.js
@@ -1,3 +1,7 @@
+import axios from 'axios';
+import qs from 'qs';
+import $ from 'jquery'
+
 var getType = function(o) {
     var type = Object.prototype.toString.call(o).toLocaleLowerCase().replace('[object ', '').replace(']', '');
     if (type === 'object' && o && o.hasOwnProperty('length')) {
@@ -10,7 +14,7 @@ function proxyJdk(lt, mobileSDK, config) {
     mobileSDK.lt = lt;
     
       //改变标题
-      mobileSDK.setTitleText = function(opt = '') {
+    mobileSDK.setTitleText = function(opt = '') {
         return document.title = opt;
     };
 
@@ -27,7 +31,29 @@ function proxyJdk(lt, mobileSDK, config) {
 
     //拍照
     mobileSDK.takeCamera = function(callback, config = { allowEdit: false }) {
-        lt.h5.call("takeOrPickPhoto", config, function(ret){
+        lt.h5.call("takePhoto", config, function(ret){
+            // 根据返回结果计算图片大小
+            function getFileSizeFromBase64 (b64) {
+                b64 = b64.substring(22)
+                var equalIndex= b64.indexOf('=')
+                if (b64.indexOf('=') > 0) {
+                    b64 = b64.substring(0, equalIndex)
+                }
+                var strLength = b64.length;
+                return parseInt((strLength - (strLength / 8) * 2) / 1024)
+            }
+
+            var result = {
+                url: ret,
+                size: getFileSizeFromBase64(ret)
+            }
+            callback && callback(result)
+        });
+    };
+
+    //选择照片
+    mobileSDK.takePhoto = function(callback, config = { allowEdit: false }) {
+        lt.h5.call("pickPhoto", config, function(ret){
             // 根据返回结果计算图片大小
             function getFileSizeFromBase64 (b64) {
                 b64 = b64.substring(22)
@@ -91,11 +117,151 @@ function proxyJdk(lt, mobileSDK, config) {
         
     };
 
-    // //上传图片到emap
+    //上传图片到emap
     mobileSDK.uploadImgsToEmap = function(opt) {
-        return bh.wisedu.uploadToEMAP(opt.host, opt.urls, opt.config || {});
-    };
+        // return bh.wisedu.uploadToEMAP(opt.host, opt.urls, opt.config || {});
+        return test(opt.host, opt.urls, opt.config || {})
+        function test(server, files, config = {}) {
+            let token = config.token
+            let scope
+            if (token) {
+              scope = token.substring(0, token.length - 1);
+            } else {
+              scope = new Date().getTime() + "" + parseInt(Math.random() * 100).toString()
+              token = scope + 1
+            }
+            
+            return new Promise((resolve, reject) => {
+                if (!server) {
+                    reject('server 地址未指定')
+                }
+                if (files.length === 0) {
+                    reject('没有可供上传的文件')
+                }
+                var storeId = 'image';
+                //ajax 提交form  
+                $.ajax({  
+                    url : server + '/sys/emapcomponent/file/uploadTempFileAsAttachment.do',   // emap自动保存为正式文件的上传接口
+                    type : "POST",  
+                    data : sumitImageFile(scope,token,storeId,files[0]),  
+                    dataType: "text",  
+                    processData : false,         // 告诉jQuery不要去处理发送的数据  
+                    contentType : false,        // 告诉jQuery不要去设置Content-Type请求头  
 
+                    success:function(data){  
+                        var fileResult = JSON.parse(data);
+
+                        console.log("----fileResult----")
+                        console.log(fileResult)
+
+                        let error = false
+                        try {
+                            if (!fileResult.success) {
+                                error = true
+                            }
+                        } catch (e) {
+                            error = true
+                        }
+                        if (error) {
+                            reject({
+                                success: false,
+                                token: "",
+                                errMsg: fileResult
+                            })
+                        } else {
+                            resolve({
+                                success: true,
+                                token,
+                                data: fileResult
+                            })
+                        }
+
+                    },  
+                    // xhr:function(){            //在jquery函数中直接使用ajax的XMLHttpRequest对象  
+                    //     var xhr = new XMLHttpRequest();  
+                    //     xhr.upload.addEventListener("progress", function(evt){  
+                    //         if (evt.lengthComputable) {  
+                    //             var percentComplete = Math.round(evt.loaded * 100 / evt.total);    
+                    //             console.log("正在提交."+percentComplete.toString() + '%');        //在控制台打印上传进度  
+                    //         }  
+                    //     }, false);  
+                    //     return xhr;  
+                    // }  
+                });  
+            })
+        }
+        /**  
+        * @param base64Codes  
+        *            图片的base64编码  
+        */  
+        function sumitImageFile(scope,fileToken,storeId,base64Codes){  
+            // var form=document.forms[0];  
+
+            var formData = new FormData();   //这里连带form里的其他参数也一起提交了,如果不需要提交其他参数可以直接FormData无参数的构造函数  
+
+            //convertBase64UrlToBlob函数是将base64编码转换为Blob  
+            formData.append("files",dataURLtoFile(base64Codes, fileToken + ".jpeg"));  //append函数的第一个参数是后台获取数据的参数名,和html标签的input的name属性功能相同  
+            formData.append("scope",scope)
+            formData.append("fileToken",fileToken)
+            formData.append("storeId",storeId)
+
+            return formData
+        }  
+        /**  
+        * 将以base64的图片url数据转换为文件 
+        * @param dataurl  base64
+        * @param filename  生成的图片名称
+        */  
+        function dataURLtoFile(dataurl, filename) { 
+            var arr = dataurl.split(','),
+            mime = arr[0].match(/:(.*?);/)[1],
+            bstr = atob(arr[1]),
+            n = bstr.length,
+            u8arr = new Uint8Array(n);
+            while (n--) {
+                u8arr[n] = bstr.charCodeAt(n);
+            }
+            return new File([u8arr], filename, {
+                type: mime
+            });
+        }
+        /**  
+        * 将以base64的图片url数据转换为Blob  --这次没用上
+        * @param urlData  
+        *            用url方式表示的base64图片数据  
+        */  
+       function convertBase64UrlToBlob(urlData){  
+        var bytes=window.atob(urlData.split(',')[1]);        //去掉url的头，并转换为byte  
+        //处理异常,将ascii码小于0的转换为大于0  
+        var ab = new ArrayBuffer(bytes.length);  
+        var ia = new Uint8Array(ab);  
+        for (var i = 0; i < bytes.length; i++) {  
+            ia[i] = bytes.charCodeAt(i);  
+        }  
+        return new Blob( [ab] , {type : 'image/jpeg'});  
+    }  
+        //创建隐藏的form表单 ---这次没用上
+        function createForm(scope,fileToken,storeId) {
+            var generateHideElement = function (name, value) {
+                var tempInput = document.createElement("input");
+                tempInput.type = "hidden";
+                tempInput.name = name;
+                tempInput.value = value;
+                return tempInput;
+            }
+
+            var form = document.createElement("form");
+            document.body.appendChild(form);
+
+            var scope = generateHideElement("scope", scope),
+            fileToken = generateHideElement("fileToken", fileToken),
+            storeId = generateHideElement("storeId", storeId);
+          
+            form.appendChild(scope);
+            form.appendChild(fileToken);
+            form.appendChild(storeId);
+        }
+    };
 }
 
 export default proxyJdk;
