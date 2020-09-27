c56eccd6da9cdcd60e540bc2bf0411ed4ac75987
luziwei <zwlu@wisedu.com>
2020-09-27 16:57:45 +0800

    project update

--- src/yb/defaults.js
+++ src/yb/defaults.js
@@ -2,5 +2,5 @@ export default {
   url: "", //获取签名url
   corp: "", //
   emapPrefixPath: "", //emap路径
-  uploadImgsToEmapUrl: "", //上传图片接口地址
+  uploadImgsToEmapUrl: "/sys/swpubapp/MobileCommon/saveFileFromYiban.do", //学工上传图片接口地址
 };
--- src/yb/proxy.js
+++ src/yb/proxy.js
@@ -1,20 +1,20 @@
 import axios from "axios";
 import { getType, convertCoords } from "../utils";
 
-export default (mobileSDK, config) => {
+function proxyJdk(mobileSDK, config) {
   console.log("yb config", config);
-  //添加platform判断，易班api里有的方法用的是browser.platform有的用的是browser.versions？？坑爹
+  //添加platform判断，易班api里有的方法用的是platform有的用的是versions？？坑爹
   browser.platform = browser.versions || {};
   mobileSDK.yb = browser;
   //覆盖易班错误处理api
-  window.onerror = (errorInfo) => {
-    console.error(errorInfo);
+  window.onerror = function (errorInfo) {
+    console.log(errorInfo);
   };
   //拍照
-  mobileSDK.takeCamera = (...args) => {
-    let callback;
+  mobileSDK.takeCamera = function () {
+    var args = arguments;
+    var callback;
     console.log("拍照");
-    console.log(args);
     // 处理参数，但是易班拍照api并不支持传递这些参数，所以没用
     if (getType(args[0]) === "boolean") {
       if (getType(args[1]) === "function") {
@@ -24,19 +24,19 @@ export default (mobileSDK, config) => {
     if (getType(args[0]) === "function") {
       callback = args[0];
     }
+    var sizeType = ["original", "compressed"];
+    if (getType(args[1]) === "array") {
+      callback = args[1];
+    }
+    if (getType(args[2]) === "array") {
+      sizeType = args[2];
+    }
     console.log(callback);
-    // let sizeType = ["original", "compressed"];
-    // if (getType(args[1]) === "array") {
-    //   sizeType = args[1];
-    // }
-    // if (getType(args[2]) === "array") {
-    //   sizeType = args[2];
-    // }
     //处理回调
-    const originCb = window.Q_camera_callback;
-    window.Q_camera_callback = (res) => {
+    var originCb = window.Q_camera_callback;
+    window.Q_camera_callback = function (res) {
       window.Q_camera_callback = originCb;
-      const result = eval("(" + res + ")");
+      var result = eval("(" + res + ")");
       console.log("result.o:", result.o);
       callback &&
         callback({
@@ -48,19 +48,20 @@ export default (mobileSDK, config) => {
     photo_fun();
   };
   //选择图片
-  mobileSDK.takePhoto = (...args) => {
-    let callback;
+  mobileSDK.takePhoto = function () {
+    var args = arguments;
+    var callback;
     console.log("选择图片");
     if (getType(args[0]) === "function") {
       callback = args[0];
     }
 
-    let limit = 1;
+    var limit = 1;
     if (getType(args[1]) === "number") {
       limit = args[1];
     }
 
-    let sizeType = ["original", "compressed"];
+    var sizeType = ["original", "compressed"];
     if (getType(args[1]) === "array") {
       sizeType = args[1];
     }
@@ -69,11 +70,11 @@ export default (mobileSDK, config) => {
     }
     console.log("limit:" + limit);
     //处理回调
-    const originCb = window.Q_camera_callback;
-    window.Q_camera_callback = (res) => {
+    var originCb = window.Q_camera_callback;
+    window.Q_camera_callback = function (res) {
       window.Q_camera_callback = originCb;
-      const result = eval("(" + res + ")");
-      console.log("result.o:", result.o);
+      var result = eval("(" + res + ")");
+      console.log(result.o);
       callback &&
         callback({
           base64: result.o,
@@ -84,20 +85,23 @@ export default (mobileSDK, config) => {
     photo_fun();
   };
   //预览图片
-  mobileSDK.preViewImages = (...args) => {
+  mobileSDK.preViewImages = function () {
     console.log("预览图片");
-    let infos = [];
+    var args = arguments;
+    var infos = [];
     if (getType(args[0]) === "array") {
       infos = args[0];
     }
-    let showIndex = 0;
+    var showIndex = 0;
     if (getType(args[1]) === "number") {
       showIndex = args[1];
     }
     if (infos && infos.length) {
-      const urls = infos.map((item) => item.url);
+      let urls = infos.map(function (item) {
+        return item.url;
+      });
       //易班预览图片api
-      const params = {
+      var params = {
         action: "yiban_vr_image",
         params: { vrurl: urls[showIndex] },
         callback: "",
@@ -106,16 +110,16 @@ export default (mobileSDK, config) => {
     }
   };
   //关闭页面
-  mobileSDK.closeWebView = () => {
+  mobileSDK.closeWebView = function () {
     console.log("关闭页面");
     back_fun();
   };
   //扫描二维码或者一维码
-  mobileSDK.scan = (callback) => {
+  mobileSDK.scan = function (callback) {
     console.log("扫码");
     //处理回调,二维码中必须包含“yiban_scan_result”标识，返回二维码字符串型内容；否则无该回调，直接以链接解析跳转
-    const originCb = window.getScanResult;
-    window.getScanResult = (res) => {
+    var originCb = window.getScanResult;
+    window.getScanResult = function (res) {
       window.getScanResult = originCb;
       console.log(res);
       callback && callback(res.replace("yiban_scan_result_", ""));
@@ -123,13 +127,13 @@ export default (mobileSDK, config) => {
     encode_fun();
   };
   //上传图片到emap
-  mobileSDK.uploadImgsToEmap = (opt) => {
-    return new Promise((resolve, reject) => {
+  mobileSDK.uploadImgsToEmap = function (opt) {
+    return new Promise(function (resolve, reject) {
       console.log("上传附件");
 
-      const serverIds = opt.urls;
+      var serverIds = opt.urls;
 
-      const uploadImg = () => {
+      var uploadImg = function () {
         let token = opt.config && opt.config.token;
         let scope;
         if (token) {
@@ -162,7 +166,7 @@ export default (mobileSDK, config) => {
             params: data,
           })
           .then(
-            (res) => {
+            function (res) {
               //res是请求的最外层返回，res.data才到数据层。
               //之前的判断是拿不到res.success这个对象的，所以会有部分（也没搞明白为什么只有部分应用暴露了这个问题）报错没有取到token
               if (
@@ -177,11 +181,11 @@ export default (mobileSDK, config) => {
               }
               resolve(res.data);
             },
-            (res) => {
+            function (res) {
               reject(res.data);
             }
           )
-          .catch((res) => {
+          .catch(function (res) {
             reject(res.data);
           });
       };
@@ -189,18 +193,22 @@ export default (mobileSDK, config) => {
     });
   };
   //改变标题
-  mobileSDK.setTitleText = (opt = "") => {};
+  mobileSDK.setTitleText = function (opt = "") {};
   //获取当前位置
-  mobileSDK.getCurrentPosition = (successCallback, errorCallback, options) => {
+  mobileSDK.getCurrentPosition = function (
+    successCallback,
+    errorCallback,
+    options
+  ) {
     console.log("获取当前位置");
     options = options || {};
 
     //处理回调,postion  Json  {"longitude":"经度坐标", "latitude":"纬度坐标", "address":"位置名称"}
-    const originCb = window.yibanhtml5location;
-    window.yibanhtml5location = (res) => {
+    var originCb = window.yibanhtml5location;
+    window.yibanhtml5location = function (res) {
       window.yibanhtml5location = originCb;
       console.log("yibanhtml5location:", res);
-      let coordsObj = {
+      var coordsObj = {
         longitude: "",
         latitude: "",
         address: "",
@@ -216,23 +224,23 @@ export default (mobileSDK, config) => {
             options.coordinate === 0 ? coordsObj : convertCoords(coordsObj),
         });
     };
-    const originErrorCb = window.onerror;
-    window.onerror = (res) => {
+    var originErrorCb = window.onerror;
+    window.onerror = function (res) {
+      window.onerror = originErrorCb;
       console.log("onerror:", res);
       errorCallback && errorCallback(res);
-      window.onerror = originErrorCb;
     };
     gethtml5location_fun();
   };
   //获取设备Id
-  mobileSDK.getDeviceId = (callback) => {
+  mobileSDK.getDeviceId = function (callback) {
     console.log("获取设备Id");
 
     if (typeof callback === "function") {
-      const params = { action: "uuid", params: {}, callback: "onlyid_back" };
+      var params = { action: "uuid", params: {}, callback: "onlyid_back" };
       //处理回调
-      const originCb = window.onlyid_back;
-      window.onlyid_back = (res) => {
+      var originCb = window.onlyid_back;
+      window.onlyid_back = function (res) {
         window.onlyid_back = originCb;
         console.log("onlyid_back:", res);
         callback({
@@ -245,4 +253,6 @@ export default (mobileSDK, config) => {
       mobile_api(JSON.stringify(params));
     }
   };
-};
+}
+
+export default proxyJdk;
